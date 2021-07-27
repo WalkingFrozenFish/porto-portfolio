8f11998e6d8189630aa4988f8f8fc82e05ea5759
WalkingFrozenFish <chachin.ol@yandex.kz>
2021-07-27 23:09:03 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    autoplay: {
+        delay: 5000,
+    },
+});
